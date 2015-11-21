8dbe35f477c4a4e955a431ee23ef7ed66f1348e9
AM <AM>
2015-11-21 15:22:13 -0800

    test commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,28 @@
 
 # I really like this class!
 
-## Write a short comment describing this function
-
+#My answers
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheinvers <- function(x, ...) {
+  inv <- x$getmean()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- mean(data, ...)
+  x$setmean(inv)
+  inv
 }
+
