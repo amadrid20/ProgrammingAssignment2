0b074ce8cb760d3b8bfb196b44b3fdbade1233d6
amadrid20 <amadrid20@gmail.com>
2015-11-20 16:07:45 -0800

    test commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# I really like this class!
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
